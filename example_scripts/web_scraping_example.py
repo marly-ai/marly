126dc57f2c0759e37d047f75016a8fee0192783c
Murtaza <murtaza@marly.ai>
2024-10-07 15:58:46 -0500

    update script and fix issue in pipeline service

--- example_scripts/web_scraping_example.py
+++ example_scripts/web_scraping_example.py
@@ -9,74 +9,49 @@ load_dotenv()
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-BASE_URL = "http://0.0.0.0:8100" # TODO: change this back
+BASE_URL = "http://localhost:8100"
+WEBSITE_URL = "https://www.forbes.com/sites/kenrickcai/2023/02/07/mindsdb-series-a-funding-ai-in-workplace/"
 
-website_url = "https://techcrunch.com/2024/08/28/san-franciscos-ai-hackathons-agency-lets-you-see-what-your-ai-agents-do/"
+SCHEMA = {
+    "Names": "The first name and last name of the company founders",
+    "Company Name": "Name of the Company",
+    "Round Size": "How much money has the company raised",
+    "Investors": "The names of the investors in the companies (names of investors and firms)",
+    "Summary": "Three sentence summary of the company"
+}
 
-def process_pdf():
-    # schema_1 = {
-    #     "Title" : "Name of the Paper or Article",
-    #     "Summary" : "Description of the paper",
-    #     "Paper Link" : "Hyperlink to the paper",
-    #     "Tweet Link" : "Hyperlink to the tweet"
-    # }
-
-    schema_2 = {
-        "Names" : "Names of the startup founders",
-        "Company Name" : "Name of the Company",
-        "Round Size" : "How much money has the company raised",
-        "Investors" : "List of investors in the company",
-        "Summary" : "Three sentence summary of the company"
-    }
-
+def process_website():
     client = Marly(base_url=BASE_URL)
 
     try:
-        pipeline_response_model = client.pipelines.create(
+        pipeline_response = client.pipelines.create(
             api_key=os.getenv("AZURE_OPENAI_API_KEY"),
             provider_model_name=os.getenv("AZURE_MODEL_NAME"),
             provider_type="azure",
-            workloads=[
-                {
-                    "data_source": "web",
-                    "documents_location": website_url,
-                    "schemas": [json.dumps(schema_2)],
-                }
-            ],
+            workloads=[{
+                "data_source": "web",
+                "documents_location": WEBSITE_URL,
+                "schemas": [json.dumps(SCHEMA)],
+            }],
             additional_params={
                 "azure_endpoint": os.getenv("AZURE_ENDPOINT"),
                 "azure_deployment": os.getenv("AZURE_DEPLOYMENT_ID"),
                 "api_version": os.getenv("AZURE_API_VERSION")
             }
         )
-        
-        logging.debug(f"Task ID: {pipeline_response_model.task_id}")
 
-        max_attempts = 5
-        attempt = 0
-        while attempt < max_attempts:
-            time.sleep(30)
-
-            results = client.pipelines.retrieve(pipeline_response_model.task_id)
-            logging.debug(f"Poll attempt {attempt + 1}: Status - {results.status}")
-
+        while True:
+            results = client.pipelines.retrieve(pipeline_response.task_id)
             if results.status == 'COMPLETED':
-                logging.debug(f"Pipeline completed with results: {results.results}")
-                return results.results
+                return json.dumps([json.loads(results.results[0].metrics[f'schema_{i}']) for i in range(len(results.results[0].metrics))])
             elif results.status == 'FAILED':
-                logging.error(f"Error: {results.error_message}")
                 return None
+            time.sleep(15)
 
-            attempt += 1
-
-        logging.warning("Timeout: Pipeline execution took too long.")
-        return None
-    #
     except Exception as e:
         logging.error(f"Error in pipeline process: {e}")
         return None
-    return None
 
 if __name__ == "__main__":
-    results = process_pdf()
-    print(results)
+    result = process_website()
+    print(result)
